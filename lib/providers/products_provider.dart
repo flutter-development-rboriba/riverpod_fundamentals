fa887029385921eae2cd5fff1462372ebeb99226
Richard <rdobmk@gmail.com>
2024-12-06 22:09:26 +0100

    Add add and remove products to cart functionality

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -1,12 +1,25 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:riverpod_files/data/dummy_data.dart';
+import 'package:riverpod_files/models/product.dart';
 
-final productsProvider = Provider(
-  (ref) {
-    return allProducts;
-  },
-);
+part 'products_provider.g.dart';
 
-final reducedProductsProvider = Provider((ref) {
+// final productsProvider = Provider(
+//   (ref) {
+//     return allProducts;
+//   },
+// );
+
+// final reducedProductsProvider = Provider((ref) {
+//   return allProducts.where((p) => p.price < 50).toList();
+// });
+
+@riverpod
+List<Product> products(ref) {
+  return allProducts;
+}
+
+@riverpod
+List<Product> reducedProducts(ref) {
   return allProducts.where((p) => p.price < 50).toList();
-});
+}
